5d657a0bd5b107758e5cff439f10eb60cd1dc054
user <askhatov90@list.ru>
2024-09-29 20:45:15 +0800

    git commit -m "Refactor password check program to use functions"

--- prac_02/scores.py
+++
@@ -1,9 +0,0 @@
-scores = int(input("Scores: "))
-if scores < 0 or scores > 100:
- print("Invalid score")
-elif scores >= 90:
- print("Excellent")
-elif scores >= 50:
- print("Pass")
-else:
- print("Bad")
