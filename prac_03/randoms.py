d721de3e7e1d1081c11662f7bcfb11a9078793bf
askhatov21 <150508656+askhatov21@users.noreply.github.com>
2024-10-06 17:19:39 +0800

    Add files via upload

---
+++ prac_03/randoms.py
@@ -0,0 +1,5 @@
+import random
+print(random.randint(5, 20)) #line 1, the smallest number was 5 and the largest was 19.
+print(random.randrange(3, 10, 2)) #Line 2, the smallest number was 3 and the largest number was 9. Line 2 cannot give the number 4.
+print(random.uniform(2.5, 5.5)) #Line 3, the smallest was 2.516 and the largest was 5.46.
+print(random.randint(1, 100))
