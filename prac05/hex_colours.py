2e629ffd38fe77651d6a30eab24a6978f042b181
user <askhatov90@list.ru>
2024-10-21 15:03:36 +0800

    git commit -m "Refactor password check program to use functions"  Complete prac 5 state names exercise

--- prac05/hex_colours.py
+++ prac05/hex_colours.py
@@ -1,4 +1,4 @@
-COLOR_TO_HEX = {"AliceBlue": "#f0f8ff",
+COLOR_TO_HEX = {"Absolute Zero": "#0048b", "AliceBlue": "#f0f8ff",
                 "Acid Green": "#b0bf1a",
                 "Alizarin crimson": "#e32636",
                 "Amaranth": "#e52b50",
@@ -8,17 +8,13 @@ COLOR_TO_HEX = {"AliceBlue": "#f0f8ff",
                 "AntiqueWhite1": '#ffefdb',
                 "AntiqueWhite2": "#eedfcc",
                 "AntiqueWhite3": "#cdc0b0"}
-
-color_name = input("Enter a color name: ").strip()
-
-while color_name !="":
+color_name = input("Enter the name: ").strip()
+while color_name != "":
     try:
-        """Convert input to case-intensive lookup (title-casing the input)"""
+        """Convert input to case-intensive lookup(title casing the input)"""
         hex_code = COLOR_TO_HEX[color_name.title()]
-        print(f'{color_name.title()} is {hex_code}')
+        print(f'{color_name} is {hex_code}')
     except KeyError:
-        print("invalid color name")
-
-    color_name = input("Enter a color name: ").strip()
-
-print("Program exited")
+        print("Invalid color")
+    color_name = input("Enter the name: ").strip()
+print("Program excited")
