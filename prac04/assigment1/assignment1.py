f8e925d95ce26f0523effe3d05c1965e9d9a3fe4
user <askhatov90@list.ru>
2024-10-26 18:49:51 +0800

    git commit -m  Add main function with menu structure.                    Add load_place function to load data from CSV file.csv                     Create recommend_place function for suggesting places.

--- prac04/assigment1/assignment1.py
+++ prac04/assigment1/assignment1.py
@@ -1,7 +1,7 @@
 import csv
 
 def main():
-MENU = " D - Display all places\n R - Recommended a random place\n A - Add a new place\n M - Mark a place as visited\n Q - quit\n"
+MENU = " D - Display all places\nR - Recommended a random place\nA - Add a new place\nM - Mark a place as visited\nQ - quit\n"
 print("Travel Tracker 1.0 - by Askhatov Amir")
 places = load_places() # Load location  from csv file
 
@@ -33,6 +33,11 @@ def load_places(filename="file.csv"):
     except FileNotFoundError:
         print(f'File {filename} not found.')
     return places
+
+def recommend_place(places):
+    """Function for recommends for random places."""
+    #Logic for recommend place
+    print("Recommendation logic not implemented yet.")
 
 
 main()
